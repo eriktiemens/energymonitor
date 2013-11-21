392f4fbcd336ae8c53cc4c04a3e4b7186cd7ad72
Erik Tiemens <erik.tiemens@tiemens-it.nl>
2013-11-21 17:07:02 +0100

    formatting changed

--- app/controllers/actual_usage_controller.rb
+++ app/controllers/actual_usage_controller.rb
@@ -2,10 +2,10 @@ class ActualUsageController < ApplicationController
   
    def actual_data
      
-     result =YoulessData.order("created_at DESC").first
+     result = YoulessData.order("created_at DESC").first
      
      respond_to do |format|
-       format.json {render :json => {"value" => result.pwr, "date"=> result.created_at.strftime("%d/%m/%Y %H:%m:%s")}}
+       format.json {render :json => {"value" => result.pwr, "date"=> result.created_at.strftime("%d/%m/%Y %H:%M:%S")}}
      end
      
    end
